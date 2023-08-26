c3b9eb45600e1baf1ee0a3f5a413b8073bf54738
KamilMarcinowski <142044400+KamilMarcinowski@users.noreply.github.com>
2023-08-26 22:58:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const locationSrc = document.getElementById("city");
 const temperatureSrc = document.getElementById("temperature")
-const selectCityBtn = document.getElementById("BtnSelectCity");
 
 const apiKey = "a9844ff28b049329fcd0a0c0a50f96c0";
 
@@ -10,23 +9,15 @@ window.onload = function(){
 
 function GetWeatherData()
 {
-    let location = localStorage.getItem("city");
-    locationSrc.innerHTML = location;
+    let location = locationSrc.innerHTML;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`,)
         .then(response => response.json())
         .then(data => {
+            locationSrc.innerHTML = data.name;
             temperatureSrc.innerHTML = data.main.temp.toFixed("0") + "°C";
         })
         .catch(error => {
             console.error("błąd", error);
         });
 }
-
-function SetCity()
-{
-    location = selectCity.value;
-    localStorage.setItem("city", location);
-}
-
-selectCityBtn.addEventListener("click", SetCity);
