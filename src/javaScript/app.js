ba6dc0feecb2907d676065a345ea2a63c2669e04
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-09-06 21:58:01 +0200

    repair

--- src/javaScript/app.js
+++ src/javaScript/app.js
@@ -1,22 +1,11 @@
-const locationSrc = document.getElementById("city");
-const temperatureSrc = document.getElementById("temperature")
+const search = document.getElementById("search");
+const city = document.querySelector(".city");
 
-const apiKey = "a9844ff28b049329fcd0a0c0a50f96c0";
-
-window.onload = function(){
-    GetWeatherData();
-}
-
-function GetWeatherData()
+function load()
 {
-    let location = "Olkusz";
+    localStorage.setItem("city", city.value);
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`,)
-        .then(response => response.json())
-        .then(data => {
-            temperatureSrc.innerHTML = data.main.temp.toFixed("0") + "°C";
-        })
-        .catch(error => {
-            console.error("błąd", error);
-        });
+    window.location.href = "src/main.html";
 }
+
+search.addEventListener("click", load)
